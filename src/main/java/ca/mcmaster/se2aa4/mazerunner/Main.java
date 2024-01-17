104e714572dc341baea044112722f1785c68606f
root <root@vedp>
2024-01-16 23:33:59 -0500

    refactor

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -3,6 +3,8 @@ package ca.mcmaster.se2aa4.mazerunner;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.lang.module.Configuration;
+import java.text.ParseException;
 
 import javax.swing.text.html.Option;
 
@@ -16,39 +18,22 @@ import org.apache.logging.log4j.Logger;
 public class Main {
 
     private static final Logger logger = LogManager.getLogger();
-
-
-
     public static void main(String[] args) {
 
-        //create the parser
         CommandLineParser  parser = new DefaultParser();
-
-        //new options object
         Options options = new Options();
 
         // set hash arg to true because program expects -i or --i to have a file name associated with it
         options.addOption("i", "input", true, "user file input field");
-
         
-
         System.out.println("** Starting Maze Runner");
+        
         try {
-            
             //parse the commandline args
             CommandLine cmd = parser.parse(options, args);
 
-            //create an empty string that will store the actual name of file that needs to be parsed
-            String fileName = "";
-
-            //check the valid options and store into fileName
-            if (cmd.hasOption("i")) {
-                fileName = cmd.getOptionValue("i");
-            } else if(cmd.hasOption("input")){
-                fileName = cmd.getOptionValue("input");
-            } else {
-                logger.error("incorrect or no command line tags entered.");
-            }
+            //create a string that will store the actual name of file that needs to be parsed
+            String fileName = createFileName(cmd);
 
             logger.info("**** Reading the maze from file " + fileName);
             BufferedReader reader = new BufferedReader(new FileReader(fileName));
@@ -70,4 +55,21 @@ public class Main {
         logger.info("PATH NOT COMPUTED");
         logger.info("** End of MazeRunner");
     }
+
+
+    private static String createFileName(CommandLine cmd){
+        //empty string
+        String fileName = "";
+        //check the valid options and store into fileName
+        if (cmd.hasOption("i")) {
+            fileName = cmd.getOptionValue("i");
+        } else if(cmd.hasOption("input")){
+            fileName = cmd.getOptionValue("input");
+        } else {
+            logger.error("incorrect or no command line tags entered.");
+        } 
+        
+        return fileName;
+    }
+    
 }
